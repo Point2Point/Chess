e09c864f124e926c83f9489aeddac735e5a79a45
AKryshtopchyk <AKryshtopchyk@ibagroup.eu>
2019-07-11 17:11:40 +0300

    #7 feat: add authorization component

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <LoginForm />
+  </div>
+</template>
+<script>
+import LoginForm from '@/components/LoginForm.vue';
+
+export default {
+  name: 'login',
+  components: {
+    LoginForm,
+  },
+};
+</script>
+<style lang="scss" scoped>
+  .md-content {
+    width: 200px;
+    height: 200px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
