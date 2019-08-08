40f9e35c3a6ffed678b30573243a49441bd115e7
Alexei <APutsykovich@iba.by>
2019-08-08 10:08:35 +0300

    #27 feat: add app toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,80 @@
 <template>
-  <div id="app">
-    <!--<div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>-->
-    <router-view/>
+  <div class="page-container">
+    <md-app md-waterfall md-mode="fixed-last">
+      <md-app-toolbar class="md-large md-dense md-primary">
+        <div class="md-toolbar-row">
+          <div class="md-toolbar-section-start">
+            <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+              <md-icon>menu</md-icon>
+            </md-button>
+
+            <span class="md-title">Chess</span>
+          </div>
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button">
+              <md-icon>more_vert</md-icon>
+            </md-button>
+          </div>
+        </div>
+
+        <div class="md-toolbar-row">
+          <md-tabs class="md-primary">
+            <md-tab id="tab-home" md-label="Home"></md-tab>
+          </md-tabs>
+        </div>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible">
+        <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+
+        <md-list>
+          <md-list-item>
+            <md-icon>move_to_inbox</md-icon>
+            <span class="md-list-item-text">Inbox</span>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content>
+        <div id="app">
+          <router-view/>
+        </div>
+
+      </md-app-content>
+    </md-app>
   </div>
 </template>
-
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    menuVisible: false,
+  }),
+};
+</script>
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  }
+
+  .md-app {
+    max-height: 100vh;
+    border: 1px solid rgba(#000, .12);
+  }
+
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
 </style>
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,14 +5,19 @@ export default {
     scene: null,
     camera: null,
     renderer: null,
-    cube: null
+    cube: null,
   },
-  created: function () {
+  created() {
+
+  },
+  mounted() {
     this.init();
     this.render();
   },
   methods: {
-    init: function () {
+    init() {
+      const container = document.querySelector('#container');
+
       this.scene = new THREE.Scene();
 
       this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -26,28 +31,24 @@ export default {
       this.renderer.setSize(window.innerWidth, window.innerHeight);
 
       this.cube = new THREE.Mesh(
-          new THREE.BoxGeometry(10 * Math.random(), 10 * Math.random(), 10 * Math.random()),
-          new THREE.MeshNormalMaterial()
+        new THREE.BoxGeometry(10 * Math.random(), 10 * Math.random(), 10 * Math.random()),
+        new THREE.MeshNormalMaterial(),
       );
 
       this.cube.name = 'cube';
       this.scene.add(this.cube);
 
-      document.body.appendChild(this.renderer.domElement);
+      container.appendChild(this.renderer.domElement);
 
       this.render();
     },
-    render: function () {
+    render() {
       this.renderer.render(this.scene, this.camera);
 
       this.scene.getObjectByName('cube').rotation.x += 0.05;
       this.scene.getObjectByName('cube').rotation.y += 0.05;
 
       requestAnimationFrame(this.render);
-    }
-  }
-}
-
-
-
-
+    },
+  },
+};
